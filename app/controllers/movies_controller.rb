e48fe96e9ffa3b2a80e5ea852fb15b8cee8ae819
Albert <aphimma2@uncc.edu>
2016-03-01 15:48:53 +0000

    Done with section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,6 +17,27 @@ def new
   # default: render 'new' template
 end
 
+# in movies_controller.rb
+def create
+  @movie = Movie.create!(params[:movie])
+  redirect_to movies_path
+end
+
+# in movies_controller.rb
+def create
+  @movie = Movie.create!(movie_params)
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+end
+
+def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+end
+
+
 private
 
     def movie_params
